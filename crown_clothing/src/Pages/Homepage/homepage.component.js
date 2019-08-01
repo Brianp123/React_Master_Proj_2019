4eacf9dd673bdd36d9dddc40864aa6217de2e623
Brian Pickard <bjpickard1@gmail.com>
2019-08-01 19:27:29 -0400

    More update to components and style

---
+++ crown_clothing/src/Pages/Homepage/homepage.component.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./homepage.styles.scss";
+import Directory from "../../Components/directory/directory.component";
+
+const HomePage = () => (
+  <div className="Homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
