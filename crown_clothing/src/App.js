e991760dd6a1f084663577f3e8d0684bbf83188a
Brian Pickard <bjpickard1@gmail.com>
2019-08-01 20:24:26 -0400

    Part 1 complete.....

--- crown_clothing/src/App.js
+++ crown_clothing/src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import HomePage from "./Pages/Homepage/homepage.component";
+import { Route, Switch } from "react-router-dom";
+import HatsPage from "./Pages/Hatspage/hatpage.component";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
+    </div>
   );
 }
 
