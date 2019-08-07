0f2935da79593942f987b987a7d8b01535d81f66
Brian Pickard <bjpickard1@gmail.com>
2019-08-07 17:47:46 -0400

    Added login form with Bulma and validation

--- crown_clothing/src/App.js
+++ crown_clothing/src/App.js
@@ -3,6 +3,7 @@ import HomePage from "./Pages/Homepage/homepage.component";
 import { Route, Switch } from "react-router-dom";
 import ShopPage from "./Pages/Shop/shop.component";
 import Header from "./Components/header/header.component";
+import SignIn from "./Pages/sign-in/sign-in-page.component";
 import "./App.css";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignIn} />
       </Switch>
     </div>
   );
---
+++ crown_clothing/src/Pages/sign-in/sign-in-page.component.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./sign-in-page.style.scss";
+import SignIn from "../../Components/sign-in/sign-in.component";
+
+const SignInPage = () => (
+  <>
+    <SignIn />
+  </>
+);
+export default SignInPage;
